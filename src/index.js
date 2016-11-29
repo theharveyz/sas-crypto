05df6cdb54831e88008a26690013ebc900375e2d
ZhangHarvey <geekwebme@gmail.com>
2016-11-29 15:03:30 +0800

    fix lint

--- src/index.js
+++ src/index.js
@@ -3,109 +3,117 @@ import crypto from 'crypto';
 
 export default class SAS {
 
-	constructor(symmetry, dilimiter) {
-		this.symmetry = symmetry || 'aes-256-cbc';
-		this.dilimiter = dilimiter || ".s.a.s.";
-	}
+  constructor(symmetry, dilimiter) {
+    this.symmetry = symmetry || 'aes-256-cbc';
+    this.dilimiter = dilimiter || '.s.a.s.';
+  }
 
-	/**
+  /**
    * @param {string|Buffer} key
    * @param {string} method
    */
-	key(key, method = 'private') {
-		this.rsakey = key instanceof Buffer ? key : typeof key == 'string' || typeof key == 'number' ? new Buffer(key) : null;
-		if(method == 'private') {
-			this.rsakey = ursa.createPrivateKey(this.rsakey);
-		} else {
-			this.rsakey = ursa.createPublicKey(this.rsakey);
-		}
-		return this;
-	}
+  key(key, method = 'private') {
+    this.rsakey = null;
+    if (key instanceof Buffer) {
+      this.rsakey = key;
+    }
+    if ((typeof key === 'string') || (typeof key === 'number')) {
+      this.rsakey = new Buffer(key);
+    }
+    if (method === 'private') {
+      this.rsakey = ursa.createPrivateKey(this.rsakey);
+    } else {
+      this.rsakey = ursa.createPublicKey(this.rsakey);
+    }
+    return this;
+  }
 
-	/**
+  /**
    * @param {string} data
    */
-	encrypt(data) {
-		if (!ursa.isKey(this.rsakey)) {
-			throw new Error("Please specify the correct key!");
-		}
-		let aesKey = this.createRandomHash();
-		const encryptedData = this.aesEncrypt(aesKey, data);
-		// 加密aesKey
-		aesKey = ursa.isPrivateKey(this.rsakey) ? this.privateRsaEncrypt(aesKey) : this.publicRsaEncrypt(aesKey);
-		return aesKey + this.dilimiter + encryptedData;
-	}
+  encrypt(data) {
+    if (!ursa.isKey(this.rsakey)) {
+      throw new Error('Please specify the correct key!');
+    }
+    let aesKey = this.createRandomHash();
+    const encryptedData = this.aesEncrypt(aesKey, data);
+    // 加密aesKey
+    aesKey = ursa.isPrivateKey(this.rsakey) ?
+      this.privateRsaEncrypt(aesKey) : this.publicRsaEncrypt(aesKey);
+    return aesKey + this.dilimiter + encryptedData;
+  }
 
-	/**
+  /**
    * @param {string} encryptedData
    */
-	decrypt(encryptedData) {
-		if (!ursa.isKey(this.rsakey)) {
-			throw new Error("Please specify the correct key!");
-		}
-		const data = encryptedData.split(this.dilimiter);
-		if (data.length != 2) {
-			throw new Error("The encrypted data is illegal!");
-		}
+  decrypt(encryptedData) {
+    if (!ursa.isKey(this.rsakey)) {
+      throw new Error('Please specify the correct key!');
+    }
+    const data = encryptedData.split(this.dilimiter);
+    if (data.length !== 2) {
+      throw new Error('The encrypted data is illegal!');
+    }
 
-		const aesKey = ursa.isPrivateKey(this.rsakey) ? this.privateRsaDecrypt(data[0]) : this.publicRsaDecrypt(data[0]);
-		return this.aesDecrypt(aesKey, data[1]);
-	}
+    const aesKey = ursa.isPrivateKey(this.rsakey) ?
+      this.privateRsaDecrypt(data[0]) : this.publicRsaDecrypt(data[0]);
+    return this.aesDecrypt(aesKey, data[1]);
+  }
 
-	/**
+  /**
    * 生成随机aes key
    */
-	createRandomHash() {
-		return crypto.randomBytes(32).toString('hex');
-	}
+  createRandomHash() {
+    return crypto.randomBytes(32).toString('hex');
+  }
 
   /**
    * aes对称加密
    */
-	aesEncrypt(key, data) {
-		const cip = crypto.createCipher(this.symmetry, key);
-		let encryptedData = '';
-		encryptedData += cip.update(data, 'utf8', 'base64');
-		encryptedData += cip.final('base64');
-		return encryptedData;
-	}
+  aesEncrypt(key, data) {
+    const cip = crypto.createCipher(this.symmetry, key);
+    let encryptedData = '';
+    encryptedData += cip.update(data, 'utf8', 'base64');
+    encryptedData += cip.final('base64');
+    return encryptedData;
+  }
 
   /**
    * aes对称解密
    */
-	aesDecrypt(key, data) {
-		const cip = crypto.createDecipher(this.symmetry, key);
-		let decrypted_data = '';
-		decrypted_data += cip.update(data, 'base64', 'utf8');
-		decrypted_data += cip.final('utf8');
-		return decrypted_data;
-	}
+  aesDecrypt(key, data) {
+    const cip = crypto.createDecipher(this.symmetry, key);
+    let decryptedData = '';
+    decryptedData += cip.update(data, 'base64', 'utf8');
+    decryptedData += cip.final('utf8');
+    return decryptedData;
+  }
 
-	/**
+  /**
    * 私钥加密
    */
-	privateRsaEncrypt(data) {
-		return this.rsakey.privateEncrypt(data, 'binary', 'base64');
-	}
+  privateRsaEncrypt(data) {
+    return this.rsakey.privateEncrypt(data, 'binary', 'base64');
+  }
 
   /**
    * 公钥解密
    */
-	publicRsaDecrypt(data) {
-		return this.rsakey.publicDecrypt(data, 'base64', 'binary');
-	}
+  publicRsaDecrypt(data) {
+    return this.rsakey.publicDecrypt(data, 'base64', 'binary');
+  }
 
-	/**
+  /**
    * 公钥加密
    */
-	publicRsaEncrypt(data) {
-		return this.rsakey.encrypt(data, 'binary', 'base64');
-	}
+  publicRsaEncrypt(data) {
+    return this.rsakey.encrypt(data, 'binary', 'base64');
+  }
 
   /**
    * 私钥解密
    */
-	privateRsaDecrypt(data) {
-		return this.rsakey.decrypt(data, 'base64', 'binary');
-	}
-};
+  privateRsaDecrypt(data) {
+    return this.rsakey.decrypt(data, 'base64', 'binary');
+  }
+}
--- test/sas.test.js
+++ test/sas.test.js
@@ -1,36 +1,36 @@
 import chai from "chai";
-import SAS from "../src/";
+import SAS from "../src";
 import fs from "fs";
 import ursa from "ursa";
 const assert = chai.assert;
 chai.should();
 
 const sas = new SAS();
-const private_key = fs.readFileSync("./test/_fixture/private.pem");
-const public_key = fs.readFileSync("./test/_fixture/public.pub");
-const data = "hello anri!"
+const privateKey = fs.readFileSync("./_fixture/private.pem");
+const publicKey = fs.readFileSync("./_fixture/public.pub");
+const data = "hello sas!"
 
-describe("anri test", () => {
+describe("sas test", () => {
 
-    it("random hash aes key length should be 64", () => {
-        let aes_key = sas._createRandomHash();
-        assert.equal(aes_key.length, 64);
-    });
+  it("random hash aes key length should be 64", () => {
+    const aesKey = sas.createRandomHash();
+    assert.equal(aesKey.length, 64);
+  });
 
-    it("specify key", () => {
-        let str = private_key.toString("utf8");
-        str.should.be.a("string");
-        assert(ursa.isPrivateKey(sas.key(str)._key), true);
-    });
+  it("specify key", () => {
+    let str = privateKey.toString("utf8");
+    str.should.be.a("string");
+    assert(ursa.isPrivateKey(sas.key(str).rsakey), true);
+  });
 
-    it("private key encrypt, public key decrypt", () => {
-        let encrypted_data = sas.key(private_key).encrypt(data);
-        assert.equal(sas.key(public_key, 'public').decrypt(encrypted_data), data);
-    });
+  it("private key encrypt, public key decrypt", () => {
+    let encryptedData = sas.key(privateKey).encrypt(data);
+    assert.equal(sas.key(publicKey, 'public').decrypt(encryptedData), data);
+  });
 
-    it("public key encrypt, private key decrypt", () => {
-        let encrypted_data = sas.key(public_key, 'public').encrypt(data);
-        assert.equal(sas.key(private_key, 'private').decrypt(encrypted_data), data);
-    });
+  it("public key encrypt, private key decrypt", () => {
+    let encryptedData = sas.key(publicKey, 'public').encrypt(data);
+    assert.equal(sas.key(privateKey, 'private').decrypt(encryptedData), data);
+  });
 });
 
